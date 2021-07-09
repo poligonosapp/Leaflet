257652dab1861aa75279b347d00670721f24f2c5
Luis Mendes <machadoluismendes@gmail.com>
2021-07-09 17:21:04 -0300

    nodejs geojson format text encoding stream exception handling
    nodejs geojson format text encoding stream exception handling

--- src/util/utfserver.ts
+++ src/util/utfserver.ts
@@ -2,15 +2,58 @@
 
 import net from 'net';
 
+try{
 
-const server = net.createServer(function (conn) {
+const chardet = require('chardet');
+
+chardet.detect(Buffer.from('{
+       "type": "Feature",
+       "bbox": [-10.0, -10.0, 10.0, 10.0],
+       "geometry": {
+           "type": "Polygon",
+           "coordinates": [
+               [
+                   [-10.0, -10.0],
+                   [10.0, -10.0],
+                   [10.0, 10.0],
+                   [-10.0, -10.0]
+               ]
+           ]
+       }
+   }'));
+// or
+chardet.detectFile('./polygons.geojson').then(encoding => console.log(encoding));
+// or
+    chardet.detectFileSync('./polygons.geojson');
+
+
+
+}catch(ex){
+
+
+    // set enconding
+    if (true) {
+    const stream = require('stream');
+
+    const server = net.createServer(function (conn) {
 
     const iconv = new Iconv('latin1', 'utf-8');
 
     conn.pipe(iconv).pipe(conn);
+    }
+    
+
+}finally{
+
+    return;
 
 });
 
 server.listen(8000);
 
 console.log('Listening on tcp://0.0.0.0:8000/');
+
+} // end finally
+
+
+
